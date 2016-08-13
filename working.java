83fa603e99401d14e028118705822b08ea0d8d22
prabaprabakaran <prabakar.431@gmail.com>
2016-08-13 00:02:28 -0700

    Create working.java

---
+++ working.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class working {
+	public static void main(String[] args) {
+	Scanner se=new Scanner(System.in);
+	String n=se.nextLine();
+	if(n.equals("sunday")){
+		System.out.println("false");
+	}
+	else{
+	System.out.println("true");
+	}}
+
+}
